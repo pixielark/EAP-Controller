c9da761eb0f2a26ccd217eb19d133d8ce44ad517
Morgan Yang <mengzhy@amazon.com>
2020-12-14 10:32:48 -0800

    update to 4.2.8

--- install.sh
+++ install.sh
@@ -100,10 +100,10 @@ chmod -R +x /etc/my_init.d/
 #########################################
 
 cd /tmp
-wget https://static.tp-link.com/2020/202011/20201110/Omada_SDN_Controller_v4.2.4_linux_x64.tar.gz
-tar zxvf Omada_SDN_Controller_v4.2.4_linux_x64.tar.gz
-chown -R root:root /tmp/Omada_SDN_Controller_v4.2.4_linux_x64
-cd Omada_SDN_Controller_v4.2.4_linux_x64
+wget https://static.tp-link.com/2020/202012/20201211/Omada_SDN_Controller_v4.2.8_linux_x64.tar.gz
+tar zxvf Omada_SDN_Controller_v4.2.8_linux_x64.tar.gz
+chown -R root:root /tmp/Omada_SDN_Controller_v4.2.8_linux_x64
+cd Omada_SDN_Controller_v4.2.8_linux_x64
 chmod +x install.sh
-sed -i '209d' install.sh
+sed -i '218d' install.sh
 echo yes | ./install.sh
